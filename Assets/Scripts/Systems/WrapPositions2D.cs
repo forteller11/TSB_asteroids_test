8e70fcb2874d37ad7e17e0353f756efa1b9ac3cc
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2022-03-17 21:40:18 -0400

    Made screen wrapping dynamic to entity size
    This is done by taking into account size of the Collider2D

--- Assets/Scripts/Systems/WrapPositions2D.cs
+++ Assets/Scripts/Systems/WrapPositions2D.cs
@@ -1,8 +1,10 @@
 using Charly.Data;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
+using Collider2D = Charly.Data.Collider2D;
 
 namespace Charly.Systems
 {
@@ -34,18 +36,30 @@ namespace Charly.Systems
                 return;
             
             //todo [Perf] look into avoiding branching in favour of modulo operator or other workarounds if this ever becomes a perf problem
-            Entities.ForEach((ref Translation translation) =>
+            Entities.ForEach((Entity entity, ref Translation translation) =>
             {
-                //this assumes that positions will never be more than worldBounds.Value.Size out of the bounds
-                if (translation.Value.x > worldBounds.Value.MaxExtents.x)
+                float2 halfSize = float2.zero;
+                if (HasComponent<Collider2D>(entity))
+                {
+                    var collider = GetComponent<Collider2D>(entity);
+                    if (collider.Type == ColliderType.Circle)
+                    {
+                        halfSize = collider.Radius;
+                    }
+                }
+
+                float2 minPos = translation.Value.xy - halfSize;
+                float2 maxPos = translation.Value.xy + halfSize;
+                
+                if (minPos.x > worldBounds.Value.MaxExtents.x)
                     translation.Value.x = worldBounds.Value.MinExtents.x + float.Epsilon;
-                else if (translation.Value.x < worldBounds.Value.MinExtents.x)
+                else if (maxPos.x < worldBounds.Value.MinExtents.x)
                     translation.Value.x = worldBounds.Value.MaxExtents.x - float.Epsilon;
 
-                if (translation.Value.y > worldBounds.Value.MaxExtents.y)
-                    translation.Value.y -= worldBounds.Value.Size.y;
-                else if (translation.Value.y < worldBounds.Value.MinExtents.y)
-                    translation.Value.y += worldBounds.Value.Size.y;
+                if (minPos.y > worldBounds.Value.MaxExtents.y)
+                    translation.Value.y = worldBounds.Value.MinExtents.y + float.Epsilon;
+                else if (maxPos.y < worldBounds.Value.MinExtents.y)
+                    translation.Value.y = worldBounds.Value.MaxExtents.y - float.Epsilon;
 
             }).ScheduleParallel();
         }
