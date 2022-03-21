feff9522555941f2328770c83c5cc06b37db2e8b
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2022-03-20 23:47:42 -0400

    Destruction system can now handle transform hierarchies one child deep
    Before this children would not be destroyed when their parents were queued for destruction.
    
    Limitations:
    Arbitrary length hierarchies are not currently supported.

--- Assets/Scripts/Systems/DestroyQueuedDestructibles.cs
+++ Assets/Scripts/Systems/DestroyQueuedDestructibles.cs
@@ -1,8 +1,10 @@
 using Charly.Data;
 using Unity.Entities;
+using Unity.Transforms;
 
 namespace Charly.Systems
 {
+    //this only handles cases with transforms of 1 child deep... arbitrary hierarchy trees not supported.
     [UpdateAfter(typeof(FindOverlapsAndQueueDestruction))]
     public class DestroyQueuedDestructibles : SystemBase
     {
@@ -14,13 +16,27 @@ namespace Charly.Systems
         
         protected override void OnUpdate()
         {
-            var commandBufferConcurrent = _endSimulationECBSystem.CreateCommandBuffer().AsParallelWriter();
-            Entities
-                .ForEach((Entity currentEntity, int entityInQueryIndex,ref Destructible destructible) =>
+            var commandBufferConcurrent1 = _endSimulationECBSystem.CreateCommandBuffer().AsParallelWriter();
+            Entities.ForEach((Entity currentEntity, int entityInQueryIndex, in Destructible destructible) =>
                 {
                     if (destructible.BeingDestroyed)
-                        commandBufferConcurrent.DestroyEntity(entityInQueryIndex, currentEntity);
+                        commandBufferConcurrent1.DestroyEntity(entityInQueryIndex, currentEntity);
                                 
+                })
+                .ScheduleParallel();
+            
+            var commandBufferConcurrent2 = _endSimulationECBSystem.CreateCommandBuffer().AsParallelWriter();
+            Entities.ForEach((Entity currentEntity, int entityInQueryIndex, in Destructible destructible, in DynamicBuffer<Child> children) =>
+                {
+                    if (destructible.BeingDestroyed)
+                    {
+                        commandBufferConcurrent2.DestroyEntity(entityInQueryIndex, currentEntity);
+                        foreach (var child in children)
+                        {
+                            commandBufferConcurrent2.DestroyEntity(entityInQueryIndex, child.Value);
+                        }
+                    }
+
                 })
                 .ScheduleParallel();
                     
